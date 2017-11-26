00a70ec380758d90831e589373146b8c7b92b9ee
agastineau <anaisgastineau6@gmail.com>
2017-11-26 16:56:09 +0100

    Add files via upload

---
+++ gaussian.m
@@ -0,0 +1,11 @@
+%function y = gaussian (x, sigma2)
+%y = exp (-sum(sum(x.^2))/(2*sigma2)); 
+function gaussian_mask = gaussian (size_window, sigma,c)
+I = diag (-size_window : size_window) * ones (2 * size_window + 1, 2 * size_window + 1);
+J = ones (2 * size_window + 1, 2 * size_window + 1) * diag (-size_window : size_window);
+gaussian_mask = exp ( -(I.^2 + J.^2) ./ (2 * sigma^2));
+if (c == 3)  
+  gaussian_mask(:,:,2) = exp ( -(I.^2 + J.^2) ./ (2 * sigma^2));
+  gaussian_mask(:,:,3) = exp ( -(I.^2 + J.^2) ./ (2 * sigma^2));
+end
+end
---
+++ gradx.m
@@ -0,0 +1,6 @@
+function M = gradx (I)
+[m,n,c] = size (I);
+M = zeros (m,n,c);
+M(1:end-1,:,:) = I(2:end,:,:) - I(1:end-1,:,:);
+end
+
