82e70f49a63556f9c925ba2a9950e4ec149bf326
xArchange <candice.bentejac@gmail.com>
2017-11-27 23:34:35 +0100

    Added random search to the patch match algorithm.
    - Works totally fine, results are really improved, but needs a more
    extensive testing to make sure there are no indices issue.
    - Needs to be parallelized or somehow sped up.

--- PatchMatch.m
+++ PatchMatch.m
@@ -77,11 +77,53 @@ function NNF = PatchMatch(A, B, patch_size, iterations)
                     end
                 end
                 
+                % Random search.
+                % Initializing the beginning of random search (max
+                % iterations).
+                rs_start = intmax;
+                if rs_start > max(m + half_patch, n + half_patch)
+                    rs_start = max(m + half_patch, n + half_patch);
+                end
+                     
+                % Hardcoded value of RAND_MAX in C++ (plays the role of an
+                % arbitrary value used to generate a random integer).
+                rand_max = 32767; 
+                while rs_start >= 1
+                    x_min = max(best_x - rs_start, 1 + half_patch);
+                    x_max = min(best_x + rs_start + 1, m + half_patch);
+                    y_min = max(best_y - rs_start, 1 + half_patch); 
+                    y_max = min(best_y + rs_start + 1, n + half_patch);                    
+                    
+                    % Generating the random candidates.
+                    xp = mod(x_min + randi(rand_max), abs(x_max - x_min));
+                    yp = mod(y_min + randi(rand_max), abs(y_max - y_min));
+                    
+                    % Handling the case where the modulo is 0 or superior
+                    % to the image's size.
+                    if xp == 0
+                        xp = 1;
+                    end
+                    if yp == 0
+                        yp = 1;
+                    end                    
+                    if xp > size(pad_B, 1) - half_patch
+                        xp = size(pad_B, 1) - half_patch;
+                    end
+                    if yp > size(pad_B, 2) - half_patch
+                        yp = size(pad_B, 2) - half_patch;
+                    end
+                        
+                    [best_x, best_y, best_guess] = ImproveGuess(pad_A, pad_B, i, j, xp, yp, best_guess, best_x, best_y, half_patch);
+                    
+                    % Updating the iteration variable (value taken from the
+                    % paper).
+                    rs_start = floor(rs_start / 2); 
+                end
+                                
                 % Saving the new nearest-neighbour.
                 NNF(i - half_patch, j - half_patch, 1) = best_x;
                 NNF(i - half_patch, j - half_patch, 2) = best_y;
-                NNF(i - half_patch, j - half_patch, 3) = best_guess;
-                
+                NNF(i - half_patch, j - half_patch, 3) = best_guess;                
             end
         end        
         k = k + 1;
