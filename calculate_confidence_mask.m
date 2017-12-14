c5857478d964e71bd81782fef22a5151483ef26d
江艺烨 <jiangyiyexmu@163.com>
2017-12-15 00:14:30 +0100

    Add files via upload

---
+++ calculate_confidence_mask.m
@@ -0,0 +1,34 @@
+%% Instruction
+%  Computes the confidence mask in such a way, that pixels outside the given mask have confidence of 1.0
+%  and inside the masked region confidence values gradually decay up to the given asymptotic value.
+
+%  @param domain: 
+%         Masked region
+%         format: Matrix, with all entries either 0 or 1. 1 stands for the
+%         pixel within inpainting domain, 0 stands for the known part of
+%         image.
+%  @param decay_time: 
+%         Controls the speed of confidence values decay
+%         format: scalar
+%  @param asymptotic_value: 
+%         Lower boundary for confidence values
+%         format: scalar
+
+% Return: confidence_mask
+%         Matrix(image)
+
+% This function need to call its auxiliary function <<distance2boundry>>
+%% 
+function confidence_mask=calculate_confidence_mask(domain,decay_time,asymptotic_value)
+
+if max(domain) == 255, domain=domain/255;end;
+
+if decay_time>0
+    dist=distance2boundry(domain);
+    confidence_mask=dist;
+    confidence_mask(domain==1)=(1-asymptotic_value)*exp(-dist(domain==1)/decay_time)+asymptotic_value;
+else
+    confidence_mask(domain==1)=asymptotic_value;
+end
+    
+end
---
+++ distance2boundry.m
@@ -0,0 +1,32 @@
+%% instruction
+% This function is the auxiliary function for function
+% <<calculate_confidence_mask>>, to get distance to the boundary
+% in advance.
+
+% For each point in the inpainting domain assigns the distance to the nearest point NOT belonging to
+% the inpainting domain(amounts to the distance to the nearest point at the
+% boundary)
+
+% Here, we only care about the points inside the inpainting domain, so as for points outside 
+% inpainting domain, to facilitate the following implement, all assigned to 1 
+%%
+function dist=distance2boundry(domain)
+ 
+dist=1-domain;
+
+se = strel('square',3);
+boundary = imdilate(domain,se) - domain;
+
+[coordinate_bound(:,1),coordinate_bound(:,2),~]=find(boundary);
+[coordinate_dom(:,1),coordinate_dom(:,2),~]=find(domain);
+
+tmp=pdist2(coordinate_bound,coordinate_dom,'Euclidean');
+tmp=min(tmp);
+
+for i=1:size(coordinate_dom(:,1),1)
+    dist(coordinate_dom(i,1),coordinate_dom(i,2))=tmp(i);    
+end
+
+dist=double(dist);
+
+end
