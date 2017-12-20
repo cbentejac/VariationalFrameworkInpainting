eeec94fb1e551fc4730efdf585114bc6614ff689
江艺烨 <jiangyiyexmu@163.com>
2017-12-20 19:53:42 +0100

    Implement Multiscale scheme in Section 3.4

--- multiscale.m
+++ multiscale.m
@@ -1,20 +1,52 @@
-function [u_l,phi_l] = multiscale (u0,Mask, size_patch, L, A, tolerance, sigma2)
-[m,n,~] = size (u0);
+function [u_l,phi_l] = multiscale(u0, Mask, size_patch, L, A, tolerance, sigma2, lambda)
+
+[m,n,channel] = size (u0);
 r = (m*n/A)^(1/(L-1));
-for l = L-2:-1:0
-    M = imresize (Mask, r);
-    u_l = imresize (u0, r);
-    if (l == L-1)
-       u_l =  mean (u0(:)) * (1-M);
-    else
-        %upsample using NN interpolation  ( PatchMatch function ?)
-        phi_l = 
-        %and scale by r
-        phi_l = imresize (phi_l,r);
-        %image update step
-        u_l = image_update (phi_l, u_l, Mask, size_patch, sigma2);
-    end
-    %solve with algo minimization energy
-    [u_l,phi_l] = MinimizationOfEnergies (u_l, tolerance, size_patch);
+
+% Construct pyramid of inpainting domain(Mask)
+kernel_size=0.62*sqrt(r^2-1);
+g_sigma = fspecial('gaussian',kernel_size);
+
+pyramid_mask{1}=Mask;
+for l=2:L
+    imblur=imfilter(pyramid_mask{l-1},g_sigma,'replicate');
+    pyramid_mask{l}=imresize(imblur,1/r,'nearest');%when scale parameter<0 , 'nearest' 
+                                                   %only removes rows and columns, while keeps the
+                                                   %values of remaining pixels the same
+    pyramid_mask{l}=pyramid_mask{l}>0.4;
 end
+
+% Construct pyramid of images
+pyramid_image{1}=u0;
+for l=2:L
+    imblur=imfilter(pyramid_image{l-1}.*pyramid_mask{l-1},g_sigma,'replicate');
+    imblur=imblur./imfilter(pyramid_mask{l-1},g_sigma,'replicate');
+    pyramid_image{l}=imresize(imblur,1/r,'nearest'); 
+end
+
+for l = L:-1:1
+    
+    M_l = pyramid_mask{l};
+    if (l == L)
+       for c=1:channel
+           u_l(:,:,c)= pyramid_image{l}(:,:,c).*~M_l+sum(sum(u0(:,:,c).*~Mask))/sum(~Mask).*M_l; %1->inpainting domain
+       end
+    else
+        %Upsample correspondence phi using NN interpolation and scale by r
+        %need to make sure the every entry of our resized correspondance
+        %map is also a integer, so Nearest Neighbour Interpolation is used(using ONLY
+        %the existing values to create new contents). 
+        phi_l = r*imresize(phi_l,r,'nearest'); 
+        
+        %initialize initial condition u_l(u_0^s in the paper) at level l using image update step with
+        %resized phi_l
+        u_hat_l = pyramid_image{l}.*M_l;
+        u_l = image_update(phi_l, u_hat_l, M_l, size_patch, sigma2);
+    end
+    
+    %solve with algorithm minimization energy
+    [u_l,phi_l] = MinimizationOfEnergies(u_l, M_l, sigma2, tolerance, lambda, size_patch);
+    
+end
+
 end
