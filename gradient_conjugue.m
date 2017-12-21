1591860311181b2fe244b4c6203147a4925817b7
agastineau <anaisgastineau6@gmail.com>
2017-12-21 18:08:59 +0100

    Add files via upload

--- gradient_conjugue.m
+++ gradient_conjugue.m
@@ -6,17 +6,17 @@ for i=1:nb_iter
    Ap = (1-lambda) * div (gradx(kz.*gradx(uz)),grady(kz.*grady(uz)))-lambda*kz.*uz;
    r_t = permute (r,[2,1,3]);
    p_t = permute (p,[2,1,3]);
-   tmp(:,:,1) = p_t(:,:,1) * Ap(:,:,1);
-   tmp(:,:,2) = p_t(:,:,2) * Ap(:,:,2);
-   tmp(:,:,3) = p_t(:,:,3) * Ap(:,:,3);
-   alpha = (r_t*r)./tmp;
+   %tmp(:,:,1) = p_t(:,:,1) * Ap(:,:,1);
+   %tmp(:,:,2) = p_t(:,:,2) * Ap(:,:,2);
+   %tmp(:,:,3) = p_t(:,:,3) * Ap(:,:,3);
+   alpha = (r_t.*r)./(p_t.*Ap);
    uz = uz + alpha.*p;
    r1 = r - alpha.*Ap;
    if (norm(rk) < epsilon)
        break;
    end
    r1_t = permute (r1,[2,1,3]);
-   beta = (r1_t*r1)./(r_t*r);
+   beta = (r1_t.*r1)./(r_t.*r);
    p = r1 + beta*p;
 end
 end
