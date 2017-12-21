58491cdbe2bf2d7788d87b7faac1176447dda4a4
xArchange <candice.bentejac@gmail.com>
2017-12-21 13:35:05 +0100

    PatchMatch for inpainting (not working so far).

---
+++ NewPatchMatch.m
@@ -0,0 +1,73 @@
+function NNF = PatchMatch(A, B, mask, patch_size, iterations, NNF)
+tic;
+    A = double(A);
+    B = double(B);
+    
+    % Initializing the virtually padded images and the half_patch variable.
+    half_patch = floor(patch_size / 2);    
+    pad_A = padarray(A, [half_patch half_patch], -1);
+    pad_B = padarray(B, [half_patch half_patch], -1);
+    
+    % Handling the input given (or not) as parameters and correcting them
+    % if required. In particular, we need an odd patch size since we are
+    % centering the patches on the pixel we are trying to fill.
+    if nargin == 5
+        % Initializing the NNF.
+        NNF = NewInitializeNNF(A, B, mask, pad_A, pad_B, half_patch);
+    end
+    
+    [m, n, ~] = size(A); 
+    
+    [row, col] = find(mask == 1);
+    inpainting_domain = [row col];
+    inpainting_domain = sortrows(inpainting_domain, 1);
+    
+    k = 1;
+    while k <= iterations 
+        disp(['Starting iteration k = ', num2str(k), ' / ', num2str(iterations)]);
+        
+        x_start = 1;
+        x_end = length(inpainting_domain);
+        x_change = 1;
+        y_change = x_change;
+        if mod(k, 2) == 0
+            x_start = x_end;
+            x_end = 1;
+            x_change = -1;
+            y_change = x_change;
+        end
+        
+        for n = x_start : x_change : x_end
+            % Current best guess.
+            i = inpainting_domain(n, 1);
+            j = inpainting_domain(n, 2);
+            [best_x, best_y, best_guess] = GetBestOffsets(NNF, i, j);
+            
+            % Propagation with absolute coordinates.
+            % Left (odd) or right (even) propagation.
+            if (i - x_change) > 0 && (i - x_change) <= m
+                xp = NNF(i - x_change, j, 1) + x_change;
+                yp = NNF(i - x_change, j, 2);
+                if xp <= size(B, 1) && xp > 0
+                    [best_x, best_y, best_guess] = ImproveGuess(pad_A, pad_B, i + half_patch, j + half_patch, xp, yp, best_guess, best_x, best_y, half_patch);
+                end
+            end
+
+            % Top (odd) or bottom (even) propagation.
+            if (j - y_change) > 0 && (j - y_change) <= n
+                xp = NNF(i, j - y_change, 1);
+                yp = NNF(i, j - y_change, 2) + y_change;
+                if yp <= size(B, 2) && yp > 0
+                    [best_x, best_y, best_guess] = ImproveGuess(pad_A, pad_B, i + half_patch, j + half_patch, xp, yp, best_guess, best_x, best_y, half_patch);
+                end
+            end
+        
+            [best_x, best_y, best_guess] = NewRandomSearch(pad_A, pad_B, mask, i + half_patch, j + half_patch, best_x, best_y, best_guess, A, half_patch);
+
+            % Updating the NNF accordingly by saving the new nearest-neighbour.
+            NNF = UpdateNNF(NNF, i, j, best_x, best_y, best_guess);                
+        end         
+        k = k + 1;
+    end
+    toc;
+end
---
+++ NewRandomSearch.m
@@ -0,0 +1,57 @@
+% Assuming the propagation phase has been done, process with the random
+% search to improve the current best offsets. To do so, select pixels
+% randomly within a decreasing window and see if they are a better fit or
+% not.
+function [best_x, best_y, best_guess] = NewRandomSearch(pad_A, pad_B, mask, i, j, best_x, best_y, best_guess, A, half_patch)
+    % Initializing the beginning of random search (max
+    % iterations).
+    [m, n, ~] = size(A);
+    rs_start = intmax;
+    if rs_start > max(m + half_patch, n + half_patch)
+        rs_start = max(m + half_patch, n + half_patch);
+    end
+
+    % Hardcoded value of RAND_MAX in C++ (plays the role of an
+    % arbitrary value used to generate a random integer).
+    rand_max = 32767; 
+    while rs_start >= 1
+        x_min = max(best_x - rs_start, 1);
+        x_max = min(best_x + rs_start, m);
+        y_min = max(best_y - rs_start, 1);
+        y_max = min(best_y + rs_start, n);
+        
+        % Generating the random candidates.
+        xp = mod(x_min + randi(rand_max), abs(x_max - x_min));
+        yp = mod(y_min + randi(rand_max), abs(y_max - y_min));
+        if xp == 0
+            xp = 1;
+        end
+        if yp == 0
+            yp = 1;
+        end
+        while mask(xp, yp) == 1
+            xp = mod(x_min + randi(rand_max), abs(x_max - x_min));
+            yp = mod(y_min + randi(rand_max), abs(y_max - y_min));
+            if xp == 0
+                xp = 1;
+            end
+            if yp == 0
+                yp = 1;
+            end
+        end
+
+        % Handling the case where the modulo is 0 or superior
+        % to the image's size.
+        size_B_x = size(pad_B, 1) - half_patch - half_patch;
+        size_B_y = size(pad_B, 2) - half_patch - half_patch;
+
+        if xp > 0 && xp <= size_B_x && yp > 0 && yp <= size_B_y
+            [best_x, best_y, best_guess] = ImproveGuess(pad_A, pad_B, i, j, xp, yp, best_guess, best_x, best_y, half_patch);
+        end
+
+        % Updating the iteration variable (value taken from the
+        % paper).
+        rs_start = floor(rs_start / 2); 
+    end
+
+end
