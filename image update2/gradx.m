d3d19b686674494d2ab602fccb564210856fcd10
agastineau <anaisgastineau6@gmail.com>
2017-12-28 08:39:00 +0100

    Update gradx.m

--- image update2/gradx.m
+++ image update2/gradx.m
@@ -3,9 +3,9 @@ function M=gradx(I)
 %Calcul le gradient en x d'une image I
 %Syntaxe: gradx(I)
 
-[m,n]=size(I);
-M=zeros(m,n);
+[m,n,c]=size(I);
+M=zeros(m,n,c);
 
-M(1:end-1,:)=I(2:end,:)-I(1:end-1,:);
+M(1:end-1,:,:)=I(2:end,:,:)-I(1:end-1,:,:);
 
 
