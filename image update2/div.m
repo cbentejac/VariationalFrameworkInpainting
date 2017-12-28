912268a4fe50d13befbc6ba6042e8e54c87859de
agastineau <anaisgastineau6@gmail.com>
2017-12-28 08:41:20 +0100

    Update div.m

--- image update2/div.m
+++ image update2/div.m
@@ -5,19 +5,19 @@ function M=div(px,py)
 %(de sorte que div=-(grad)^*)
 %Syntaxe: div(px,py)
 
-[m,n]=size(px);
+[m,n,c]=size(px);
 
-M=zeros(m,n);
+M=zeros(m,n,c);
 Mx=M;
 My=M;
 
-Mx(2:m-1,:)=px(2:m-1,:)-px(1:m-2,:);
-Mx(1,:)=px(1,:);
-Mx(m,:)=-px(m-1,:);
+Mx(2:m-1,:,:)=px(2:m-1,:,:)-px(1:m-2,:,:);
+Mx(1,:,:)=px(1,:,:);
+Mx(m,:,:)=-px(m-1,:,:);
 
-My(:,2:n-1)=py(:,2:n-1)-py(:,1:n-2);
-My(:,1)=py(:,1);
-My(:,n)=-py(:,n-1);
+My(:,2:n-1,:)=py(:,2:n-1,:)-py(:,1:n-2,:);
+My(:,1,:)=py(:,1,:);
+My(:,n,:)=-py(:,n-1,:);
 
 M=Mx+My;
 
