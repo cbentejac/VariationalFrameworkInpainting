be1df179b0bb9ca003674b1bb6e4fcc5aebdc3e3
agastineau <anaisgastineau6@gmail.com>
2017-12-28 08:39:42 +0100

    Update grady.m

--- image update2/grady.m
+++ image update2/grady.m
@@ -3,9 +3,9 @@ function M=grady(I)
 %Calcul le gradient en y d'une image I
 %Syntaxe: grady(I)
 
-[m,n]=size(I);
-M=zeros(m,n);
+[m,n,c]=size(I);
+M=zeros(m,n,c);
 
-M(:,1:end-1)=I(:,2:end)-I(:,1:end-1);
+M(:,1:end-1,:)=I(:,2:end,:)-I(:,1:end-1,:);
 
 
