99230d6f993e4721d0cdef272dac2d8a8b921743
xArchange <candice.bentejac@gmail.com>
2017-12-30 01:25:41 +0100

    Fixed parallelized Patch Match.
    Performance decreasing is sometimes visible, and the speed gain is not
    as great as before, but it still exists.

--- ParallelizedPatchMatch.m
+++ ParallelizedPatchMatch.m
@@ -1,20 +1,29 @@
 % Given two images A and B, returns the nearest-neighbour field using the
 % parallelized patch match method.
-function NNF = ParallelizedPatchMatch(A, B, half_patch, iterations, NNF)
-    tic;
+function NNF = ParallelizedPatchMatch(A, B, mask, half_patch, iterations, error, lambda, NNF)
+tic;
     A = double(A);
     B = double(B);
     
+    % Initializing the virtually padded images and the half_patch variable.
     pad_A = padarray(A, [half_patch half_patch], -1);
-    pad_B = padarray(B, [half_patch half_patch], -1);    
-
-    % Handling the NNF initialization if the NNF is not already provided
-    % with.
-    if nargin == 4
+    pad_B = padarray(B, [half_patch half_patch], -1);
+    pad_B(mask == 1) = -1;
+    
+    % Handling the input given (or not) as parameters and correcting them
+    % if required. In particular, we need an odd patch size since we are
+    % centering the patches on the pixel we are trying to fill.
+    if nargin == 7
         % Initializing the NNF.
-        NNF = InitializeNNF(A, B, pad_A, pad_B, half_patch);
+        NNF = InitializeNNF(A, mask, pad_A, pad_B, half_patch, error, lambda);
     end
-
+    
+    [h, w, ~] = size(A); 
+    
+    [row, col] = find(mask == 1);
+    inpainting_domain = [row col];
+    inpainting_domain = sortrows(inpainting_domain, 1);
+    
     % For hyperthreaded CPUs to work, we need to use a cluster profile and
     % not the local ones which is set to the number of physical cores...
     %     nb_cores = strsplit(evalc('feature(''numcores'')'));
@@ -23,67 +32,77 @@ function NNF = ParallelizedPatchMatch(A, B, half_patch, iterations, NNF)
     % Gets the number of available physical cores for parallelization and
     % start the parallel pool.
     nb_cores = maxNumCompThreads;
-    size_chunk = floor(size(A, 1) / nb_cores);
+    size_chunk = floor(size(inpainting_domain, 1) / nb_cores);
     p = gcp; 
     
     % Splitting the image into chunks according to the number of CPUs.
     for i = 1 : nb_cores
         if i == nb_cores
-            chunk{i} = A((i - 1) * size_chunk + 1 : end, :, :);
-            NNF_chunk{i} = NNF((i - 1) * size_chunk + 1 : end, :, :);
+            chunk{i} = inpainting_domain((i - 1) * size_chunk + 1 : end, :);            
         else
-            chunk{i} = A((i - 1) * size_chunk + 1 : i * size_chunk, :, :);
-            NNF_chunk{i} = NNF((i - 1) * size_chunk + 1 : i * size_chunk, :, :);
+            chunk{i} = inpainting_domain((i - 1) * size_chunk + 1 : i * size_chunk, :);
         end
-        pad_chunk{i} = padarray(chunk{i}, [half_patch half_patch], -1);
     end
-    
+            
     k = 1;
     while k <= iterations 
         disp(['Starting iteration k = ', num2str(k), ' / ', num2str(iterations)]);        
         
         parfor N = 1 : nb_cores
-            % Gets the propagation limits and order for the current chunk.
-            [x_start, x_end, x_change, y_start, y_end, y_change] = GetPropapagationLimits(k, chunk{N}, half_patch);                        
-            [m, n, ~] = size(chunk{N});
+            NNF_chunk{N} = NNF;
             
-            for i = x_start : x_change : x_end
-                for j = y_start : y_change : y_end
-                    
-                    % Current best guess.
-                    [best_x, best_y, best_guess] = GetBestOffsets(NNF_chunk{N}, i - half_patch, j - half_patch);                                      
-                    
-                    % Propagation with absolute coordinates.
-                    % Left (odd) or right (even) propagation.
-                    if (i - half_patch - x_change) > 0 && (i - x_change - half_patch) <= m
-                        xp = NNF_chunk{N}(i - half_patch - x_change, j - half_patch, 1) + x_change;
-                        yp = NNF_chunk{N}(i - half_patch - x_change, j - half_patch, 2);
-                        if xp <= size(B, 1) && xp > 0
-                            [best_x, best_y, best_guess] = ImproveGuess(pad_chunk{N}, pad_B, i, j, xp, yp, best_guess, best_x, best_y, half_patch);
-                        end
+            % Gets the propagation limits and order for the current chunk.
+            x_start = 1;
+            x_end = length(chunk{N});
+            x_change = 1;
+            y_change = x_change;
+            if mod(k, 2) == 0
+                x_start = x_end;
+                x_end = 1;
+                x_change = -1;
+                y_change = x_change;
+            end
+            
+            for n = x_start : x_change : x_end
+                % Current best guess.
+                i = chunk{N}(n, 1);
+                j = chunk{N}(n, 2);
+                [best_x, best_y, best_guess] = GetBestOffsets(NNF, i, j);
+                
+                % Propagation (top or bottom).
+                if i - x_change > 0 && i - x_change <= h
+                    xp = NNF(i - x_change, j, 1);
+                    yp = NNF(i - x_change, j, 2);
+                    if xp <= size(B, 1) && xp > 0
+                        [best_x, best_y, best_guess] = ImproveGuess(pad_A, pad_B, i + half_patch, j + half_patch, xp, yp, best_guess, best_x, best_y, half_patch, error, lambda);
                     end
-
-                    % Top (odd) or bottom (even) propagation.
-                    if (j - half_patch - y_change) > 0 && (j - y_change - half_patch) <= n
-                        xp = NNF_chunk{N}(i - half_patch, j - half_patch - y_change, 1);
-                        yp = NNF_chunk{N}(i - half_patch, j - half_patch - y_change, 2) + y_change;
-                        if yp <= size(B, 2) && yp > 0
-                            [best_x, best_y, best_guess] = ImproveGuess(pad_chunk{N}, pad_B, i, j, xp, yp, best_guess, best_x, best_y, half_patch);
-                        end
-                    end
-
-                    [best_x, best_y, best_guess] = RandomSearch(pad_chunk{N}, pad_B, i, j, best_x, best_y, best_guess, chunk{N}, half_patch);
-
-                    % Updating the NNF accordingly by saving the new nearest-neighbour.
-                    NNF_chunk{N} = UpdateNNF(NNF_chunk{N}, i - half_patch, j - half_patch, best_x, best_y, best_guess);                       
                 end
+
+                % Propagation (left or right).
+                if j - y_change > 0 && j - y_change <= w
+                    xp = NNF(i, j - y_change, 1);
+                    yp = NNF(i, j - y_change, 2);
+                    if yp <= size(B, 2) && yp > 0
+                        [best_x, best_y, best_guess] = ImproveGuess(pad_A, pad_B, i + half_patch, j + half_patch, xp, yp, best_guess, best_x, best_y, half_patch, error, lambda);
+                    end
+                end
+            
+                [best_x, best_y, best_guess] = RandomSearch(pad_A, pad_B, mask, i + half_patch, j + half_patch, best_x, best_y, best_guess, A, half_patch, error, lambda);
+            
+                % Updating the parallel NNF accordingly by saving the new nearest-neighbour.
+                NNF_chunk{N} = UpdateNNF(NNF_chunk{N}, i, j, best_x, best_y, best_guess);     
             end
         end
         
-        % Update the global NNF.
-        NNF = cat(1, NNF_chunk{:});
-
+        % Updating the actual NNF using the activated parallel NNF pixels.
+        NNF_copy = NNF;
+        for i = 1 : nb_cores            
+            NNF_copy(NNF_chunk{i} ~= NNF) = NNF_chunk{i}(NNF_chunk{i} ~= NNF);
+        end
+        
+        NNF = NNF_copy;
+        
         k = k + 1;
-    end  
-    toc;
+    end
+toc;
 end
